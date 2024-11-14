30a1d9fa7c316c916d109f451853804f5120441c
chumarjamil <imumarj@gmail.com>
2024-11-14 11:49:44 +0700

    CRUD API

--- main.go
+++ main.go
@@ -24,9 +24,9 @@ func main() {
 	
 	// Define routes
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	// router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
     router.HandleFunc("/api/books", createBook).Methods("POST")
-    // router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+    router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	fmt.Println("Server is running on PORT: 9090")
 
@@ -39,11 +39,39 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+// create new book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	json.NewDecoder(r.Body).Decode(&book)
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
+}
+
+// get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for _, item := range books {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    http.Error(w, "Book not found", http.StatusNotFound)
+}
+
+// delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for index, item := range books {
+        if item.ID == params["id"] {
+            books = append(books[:index], books[index+1:]...)
+            json.NewEncoder(w).Encode(books)
+            return
+        }
+    }
+    http.Error(w, "Book not found", http.StatusNotFound)
 }
 
